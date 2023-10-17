7a6a66f175529c7625cd0e4bd1a391ce9b31d9f2
dima.avdeev <dima.avdeev@jetbrains.com>
2023-10-18 01:25:18 +0400

    run

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,20 +2,32 @@ import UIKit
 import SwiftUI
 import shared
 
-struct ComposeView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UIViewController {
-        Main_iosKt.MainViewController()
-    }
-
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
-}
-
 struct ContentView: View {
-    var body: some View {
-        ComposeView()
-                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
-    }
+    @State private var navigation = NavigationPath()
+
+        var body: some View {
+            NavigationStack(path: $navigation) {
+                CoffeeSelectorsMobileRepresentable2(
+                    onComposeClick: {
+                        navigation.append(0)
+                    }
+                )
+                .navigationDestination(for: Int.self) { destination in
+                    Text("Press back to reproduce bug")
+                }
+                .ignoresSafeArea()
+            }
+        }
 }
 
+public struct CoffeeSelectorsMobileRepresentable2: UIViewControllerRepresentable {
+    let onComposeClick: () -> Void
 
+    public func makeUIViewController(context: Context) -> UIViewController {
+        return CoffeeSelectorsMobileUIViewController2().make(onComposeClick: onComposeClick)
+    }
 
+    public func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+
+    }
+}
