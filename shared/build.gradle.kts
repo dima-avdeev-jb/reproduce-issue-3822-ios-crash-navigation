206d88fa36e53ebf92c2edb85617cc93c2591e7f
dima.avdeev <dima.avdeev@jetbrains.com>
2023-10-18 03:08:39 +0400

    reproduce

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,12 +1,9 @@
 plugins {
     kotlin("multiplatform")
-    id("com.android.library")
     id("org.jetbrains.compose")
 }
 
 kotlin {
-    androidTarget()
-
     jvm("desktop")
 
     listOf(
@@ -30,13 +27,6 @@ kotlin {
                 implementation(compose.components.resources)
             }
         }
-        val androidMain by getting {
-            dependencies {
-                api("androidx.activity:activity-compose:1.7.2")
-                api("androidx.appcompat:appcompat:1.6.1")
-                api("androidx.core:core-ktx:1.10.1")
-            }
-        }
         val iosX64Main by getting
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
@@ -51,26 +41,6 @@ kotlin {
                 implementation(compose.desktop.common)
             }
         }
-    }
-}
-
-android {
-    compileSdk = (findProperty("android.compileSdk") as String).toInt()
-    namespace = "com.myapplication.common"
-
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
-    defaultConfig {
-        minSdk = (findProperty("android.minSdk") as String).toInt()
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlin {
-        jvmToolchain(17)
     }
 }
 
