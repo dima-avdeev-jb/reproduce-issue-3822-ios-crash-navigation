5cbc643e9701076c7f529595273ba109ae196a7e
dima.avdeev <dima.avdeev@jetbrains.com>
2023-10-18 10:04:46 +0400

    minify

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,22 +1,14 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.Typography
-import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.platform.Font
 import androidx.compose.ui.unit.dp
@@ -25,15 +17,13 @@ import kotlin.io.encoding.ExperimentalEncodingApi
 
 @Composable
 fun App(onComposeClick: () -> Unit) {
-    MaterialTheme {
-        Box(Modifier.windowInsetsPadding(WindowInsets.systemBars)) {
-            Box(Modifier.width(250.dp)
-                .background(Color.Yellow)
-                .clickable { onComposeClick() }
-            ) {
-                Text("CLICK ME MULTIPLE TIMES", fontFamily = ledFontFamily3)
-            }
-        }
+    Box(
+        Modifier.windowInsetsPadding(WindowInsets.systemBars)
+            .background(Color.White)
+            .padding(10.dp)
+            .clickable { onComposeClick() }
+    ) {
+        Text("CLICK ME MULTIPLE TIMES", fontFamily = ledFontFamily3)
     }
 }
 
